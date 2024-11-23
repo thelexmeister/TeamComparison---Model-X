8166d3bf8a0787d8988767301d7d624ce9596ae4
thelexmeister <88957860+thelexmeister@users.noreply.github.com>
2024-11-23 12:59:56 -0700

    Update TeamComparison_app.py

--- TeamComparison_app.py
+++ TeamComparison_app.py
@@ -64,7 +64,14 @@ def plot_player_scores(players, team_name=""):
     return fig
 
 # Streamlit User Interface
-st.title('NFL Fantasy Team Prediction Dashboard')
+st.title('Western Wolves: NFL Fantasy Team Prediction Dashboard')
+st.text('All you have to do is start typing the name of your player in each slot and then click on it.') 
+st.text('For the flex players, just type the first one, click on it. Click again in the space, type your second name until you find the player') 
+st.text('Then click outside the box when you have 2 names in red in the box.')
+st.text(' ')
+st.text(' ')
+st.text('In the figure below, RED means high confidence in the prediction and zone of probability, BLUE means moderate confidence,
+        GREEN means low confidence.')
 
 # Create two columns for layout
 col1, col2 = st.columns(2)
